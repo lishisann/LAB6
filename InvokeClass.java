42bbe65a03b5d5fb657ceab26ddfc30521f27af1
lishisann <and.lyubov31@gmail.com>
2025-12-01 07:49:06 +0500

    Update and rename DemoClass.java to InvokeClass.java

---
+++ InvokeClass.java
@@ -0,0 +1,18 @@
+package LAB6;
+
+public class InvokeClass {
+
+    @Invoke
+    public void hello() {
+        System.out.println("Invoke вызван.");
+    }
+
+    public void notInvoked() {
+        System.out.println("Я не должен быть вызван.");
+    }
+
+    @Invoke
+    private void secret() {
+        System.out.println("Приватный invoke вызван.");
+    }
+}
